a6c052004b31dae58b755274595d8a5df05e93c8
Antonio De Luca <antonio.deluca96@gmail.com>
2022-04-05 22:11:32 +0200

    Adesso prende tutti i treni di una giornata. Li salva in un file .json

--- script.js
+++ script.js
@@ -1,41 +1,119 @@
 const Trenitalia = require("api-trenitalia");
-const Moment = require('moment');
+const moment = require('moment');
+const fs = require('fs');
+
+// Some constants
+const MINHOUR = "6";
+const MAXHOUR = "23";
+const trenitalia = new Trenitalia();
+
+/*  Nota: Le soluzione ricevute possono essere duplicate!
+    TODO: Trovare il modo per collegare la soluzione al sito di Trenitalia
+    TODO: Implementare la possibilitaa'di scegliere o meno di avere cambi
+    TODO: 
+*/
 
 
-async function getWeekTrains(stationFromName, stationToName, startDate, startHour) {
+async function getWeekTrains(stationFromName, stationToName, startDate, days) {
 
-
-    const t = new Trenitalia();
-    const stations_from = await t.autocomplete(stationFromName);
+    days = 1; // Debug purpose ELIMINATE
+    const stations_from = await trenitalia.autocomplete(stationFromName);
     const station_from = stations_from[0].name;
-    const stations_to = await t.autocomplete(stationToName);
+    const stations_to = await trenitalia.autocomplete(stationToName);
     const station_to = stations_to[0].name;
-    var startDate = Moment(startDate);
+
+    var startDate = moment(startDate);
     var dates = new Array();
-    for (var i = 1; i < 8; i++) {
-        dates.push(startDate.add(1, 'days').format("DD/MM/YYYY"));
+    var solutionsEachDay = new Object();
+    for (var i = 1; i <= days; i++) {
+        //dates.push(startDate.add(1, 'days').format("DD/MM/YYYY"));
+        let date = startDate.add(i, 'days').format("DD/MM/YYYY");
+        solutionsEachDay[date] = await getTrainsForTheDay(date);
     }
 
-    var solutionsPerDay = new Object();
-    for (const date of dates) {
-        solutionsPerDay[date] = await t.getOneWaySolutions(station_from, station_to, date, startHour, 1, 0,false,false,null);
-        // solutions = await t.getOneWaySolutions(station_from, station_to, element, startHour, 1, 0);
-        // for (const solution of solutions) {
-        //     console.log(`Treno:\t${solution.trainlist[0].trainidentifier}`);
-        //     console.log(`Part:\t${new Date(solution.departuretime)}`);
-        //     console.log(`Arri:\t${new Date(solution.arrivaltime)}`);
-        //     console.log(`Prezzo:\t${solution.originalPrice} €`);
-        //     console.log("-----------------------------------");
+
+
+    // for (const date of dates) {
+    //     //solutionsPerDay[date] = await trenitalia.getOneWaySolutions(station_from, station_to, date, startHour, 1, 0, false, false, null);
+    //     //solutionsPerDay[date] = filterAndSort(solutionsPerDay[date]);
+    //     //tranformTimeToReadable(solutionsPerDay[date]);
+
+
+    // }
+    return solutionsEachDay;
+
+
+
+
+    async function getTrainsForTheDay(date) {
+        console.log("Cercando i treni nel giorno: " + date);
+        let hour = MINHOUR;
+        let daySolutions = new Array();
+        let nReq = 0;
+        let lastReqTrain;
+        const MAXREQUEST = 10;
+        let isEndDay = false;
+        while (nReq <= MAXREQUEST && parseInt(hour) < parseInt(MAXHOUR) && !isEndDay) {
+            nReq++;
+            let temp = await trenitalia.getOneWaySolutions(station_from, station_to, date, hour, 1, 0, false, false, null);
+            daySolutions.push(temp);
+            let lastTrain = temp[temp.length-1];
+            hour = moment(lastTrain.departuretime).format("HH"); // get last hour of last result
+            if(moment(lastTrain.departuretime).format("DD/MM/YYYY") !== date)
+                isEndDay = true;
+            if(lastReqTrain != undefined && lastReqTrain.departuretime == lastTrain.departuretime){
+                throw 'Loop Detected! In getAlldayTrain() with date = ' + date;
+            }
+            lastReqTrain = lastTrain;
+        }
+        daySolutions = uniqBy(daySolutions.flat(),(key) => key.trainlist[0].trainidentifier);
+        return tranformTimeToReadable(daySolutions);
+        
+
+        // Funzione usata per ottenere array con valori unici
+        function uniqBy(a, key) {
+            var seen = {};
+            return a.filter(function(item) {
+                var k = key(item);
+                return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+            })
+        }
     }
-    return solutionsPerDay;
-    //console.log(solutions);
 
 }
 
 
-var startDate = Date();
-var date = new Date();
+function filterAndSort(solutionsArray) {
+    return solutionsArray.filter(a => a.changesno == 0)//.sort((a, b) => a.originalPrice - b.originalPrice);
+}
 
-getWeekTrains("Bologna", "Foggia", new Date(), "13").then(solutions => {
-    console.log(solutions);
-});
+function tranformTimeToReadable(solutionsArray) {
+    solutionsArray.forEach(solution => {
+        solution.readableDeparturetime = moment(solution.departuretime).format("DD/MM HH:mm");
+        solution.readableArrivaltime = moment(solution.arrivaltime).format("DD/MM HH:mm");
+    });
+    return solutionsArray;
+}
+
+async function saveToJSON(nameFile, obj) {
+    fsPromises = fs.promises;
+    await fsPromises.writeFile(nameFile, JSON.stringify(obj, null, "\t"), 'utf8');
+    console.log("JSON file has been saved.");
+}
+
+async function showResult() {
+    var startDate = Date();
+    var date = new Date();
+    const corseTrovate = await getWeekTrains("Bologna", "Foggia", new Date().setMonth(4), 1 );
+    saveToJSON("test/test.json", corseTrovate);
+    for (giornata in corseTrovate) {
+        console.log("Giorno: " + giornata);
+        console.log(corseTrovate[giornata],null,"\t");
+        console.log("------------------------");
+    }
+}
+
+
+showResult();
+console.log("Fine");
+
